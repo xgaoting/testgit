041f235f22b9232347e70f48d429ca79f3512ad1
gaoting <xgaoting@126.com>
2016-11-19 08:15:21 +0800

    瀑布流

--- echina/HTML5课程学习/jQuery/jQuery瀑布流/app.js
+++ echina/HTML5课程学习/jQuery/jQuery瀑布流/app.js
@@ -4,8 +4,27 @@
 $(document).ready(function () {
     $(window).on("load",function(){//监听load事件
         imgLocation();
+        var dataImg = {"data":[{"src":"200-1.jpg"},{"src":"200-2.jpg"},{"src":"200-3.jpg"},{"src":"200-4.jpg"},{"src":"250-1.jpg"},{"src":"250-2.jpg"},{"src":"250-3.jpg"},{"src":"250-4.jpg"}]}
+        window.onscroll = function () {
+            if(scrollside()){
+                $.each(dataImg.data,function(index,value){
+                    var box = $("<div>").addClass("box").appendTo($("#container"));//创建class为box的div并将其添加到container当中
+                    var content = $("<div>").addClass("content").appendTo(box);//创建class为content的div并将其添加到box当中
+                    console.log("./test/"+$(value));
+                    $("<img>").attr("src","./test/"+$(value).attr("src")).appendTo(content);//创建图片，并将其添加到content当中
+                });
+                imgLocation();//重新按照瀑布流的方式进行摆放
+            }
+        }
     })
 })
+function scrollside(){
+    var box = $(".box");//获取box对象
+    var lastboxHeight = box.last().get(0).offsetTop +Math.floor(box.last().height()/2);/*加载图片的界线*/
+    var documentHeight = $(document).width();//浏览器的高度
+    var scrollHeight = $(document).scrollTop();//滚动条的高度
+    return (lastboxHeight<documentHeight+scrollHeight)?true:false;//判断（什么时候进行滚动加载）
+}
 function imgLocation(){
     var box = $(".box");//获取box对象
     var boxWidth = box.eq(0).width();//获取每一个等宽盒子的宽度
